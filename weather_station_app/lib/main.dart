5779643ffe42715bc0f99eb3079699a19986869c
isaacscardoso <isaacscardosoblues@outlook.com>
2023-03-10 14:48:54 -0300

    Turning the main method into asynchronous to be able to load the environment variable '.env'.

--- weather_station_app/lib/main.dart
+++ weather_station_app/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import './pages/pages.dart';
 
-void main() => runApp(const WeatherApp());
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const WeatherApp());
+}
 
 class WeatherApp extends StatelessWidget {
   const WeatherApp({super.key});
