f0139c3c32ac437584106d2cefe019227d8e0769
isaacscardoso <isaacscardosoblues@outlook.com>
2023-03-20 16:43:55 -0300

    Refactoring in the 'responseHandle' method.

--- weather_station_app/lib/infrastructure/http/http_response_handle.dart
+++ weather_station_app/lib/infrastructure/http/http_response_handle.dart
@@ -6,9 +6,8 @@ import '../../data/http/http.dart';
 
 dynamic responseHandle(Response response) {
   final int statusCode = response.statusCode;
-  final String body = response.body;
   final Map<int, dynamic> treatmentForEachStatusCode = {
-    200: body.isNotEmpty ? jsonDecode(body) : null,
+    200: response.body.isNotEmpty ? jsonDecode(response.body) : null,
     204: null,
   };
 
