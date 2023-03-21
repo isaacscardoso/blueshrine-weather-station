c383d552420ae9365d92332b7dccc5f63a74f4b2
isaacscardoso <isaacscardosoblues@outlook.com>
2023-03-21 20:13:11 -0300

    Implementation of the 'fetchWeather' method of the remote class 'WeatherRepository'.

--- weather_station_app/lib/data/repositories/remote_weather_repository.dart
+++ weather_station_app/lib/data/repositories/remote_weather_repository.dart
@@ -1,9 +1,37 @@
 import '../../domain/repositories/repositories.dart';
 import '../../domain/entities/entities.dart';
+import '../../domain/usecases/usecases.dart';
+
+import '../usecases/usecases.dart';
 
 class RemoteWeatherRepository implements WeatherRepository {
+  final RemoteMeteorology remoteMeteorology;
+  final RemoteGeolocation remoteGeolocation;
+
+  RemoteWeatherRepository({
+    required this.remoteMeteorology,
+    required this.remoteGeolocation,
+  });
+
   @override
-  Future<WeatherEntity> fetchWeather(String city) async {
-    throw UnimplementedError();
+  Future<WeatherEntity> fetchWeather(String cityName) async {
+    final MeteorologyParameters meteorologyParameters = MeteorologyParameters(
+      cityName: cityName,
+    );
+
+    final DirectGeocodingEntity meteorology =
+        await remoteMeteorology.getGeolocationData(
+      parameters: meteorologyParameters,
+    );
+
+    final GeolocationParameters geolocationParameters = GeolocationParameters(
+      latitude: meteorology.latitude,
+      longitude: meteorology.longitude,
+    );
+
+    final WeatherEntity weatherEntity = await remoteGeolocation.getWeatherData(
+      parameters: geolocationParameters,
+    );
+    return weatherEntity;
   }
 }
