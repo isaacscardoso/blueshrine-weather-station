fdc4cbdd9877e3d6767e315e61545e1b6ad7c3ad
isaacscardoso <isaacscardosoblues@outlook.com>
2023-03-21 20:11:15 -0300

    Changing the name of the parameter 'city' to 'cityName' in the method of the abstract class 'WeatherRepository'.

--- weather_station_app/lib/domain/repositories/weather_repository.dart
+++ weather_station_app/lib/domain/repositories/weather_repository.dart
@@ -1,5 +1,5 @@
 import '../entities/entities.dart';
 
 abstract class WeatherRepository {
-  Future<WeatherEntity> fetchWeather(String city);
+  Future<WeatherEntity> fetchWeather(String cityName);
 }
