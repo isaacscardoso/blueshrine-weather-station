82efe5daa4d755e289da1327384e6899711e704a
isaacscardoso <isaacscardosoblues@outlook.com>
2023-03-23 04:14:34 -0300

    Implementation of the URI generating methods: generating URI to obtain the Geolocation data and generating URI to obtain the Weather data.

--- weather_station_app/lib/factories/http/api_url_factory.dart
+++ weather_station_app/lib/factories/http/api_url_factory.dart
@@ -1,7 +1,37 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 String makeApiUrl({
   required String scheme,
   required String subdomain,
   String? path,
 }) {
   return '$scheme://$subdomain.openweathermap.org/$path';
+}
+
+String meteorologyUrlWithParameters({
+  required String url,
+  required String cityName,
+  required String limit,
+}) {
+  Map<String, dynamic> parameters = {
+    'q': cityName,
+    'limit': limit,
+    'appid': dotenv.env['APPID'],
+  };
+  Uri uri = Uri.parse(url);
+  return uri.replace(queryParameters: parameters).toString();
+}
+
+String geolocationUrlWithParameters({
+  required String url,
+  required double latitude,
+  required double longitude,
+}) {
+  Map<String, dynamic> parameters = {
+    'lat': latitude.toString(),
+    'lon': longitude.toString(),
+    'appid': dotenv.env['APPID'],
+  };
+  Uri uri = Uri.parse(url);
+  return uri.replace(queryParameters: parameters).toString();
 }
